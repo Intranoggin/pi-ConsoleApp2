30009730193cbdae4524d99bf26419a0f3675f25
Ryan Miller <ryan@intranoggin.com>
2025-01-17 13:41:10 -0500

    added console write for state and adjusted pin

--- pi-ConsoleApp2/Program.cs
+++ pi-ConsoleApp2/Program.cs
@@ -3,7 +3,7 @@ using System.Threading;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("VS - Hello, World!");
 
-GpioController controller = new GpioController(PinNumberingScheme.Board);
+GpioController controller = new GpioController(PinNumberingScheme.Logical);
 
 var pin = 5;
 var lightTime = 300;
@@ -14,8 +14,10 @@ try
 {
     while (true)
     {
+        Console.WriteLine("Light on");
         controller.Write(pin, PinValue.High);
         Thread.Sleep(lightTime);
+        Console.WriteLine("Light off");
         controller.Write(pin, PinValue.Low);
         Thread.Sleep(lightTime);
     }
