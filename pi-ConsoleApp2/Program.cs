df410a864ab0aa5af205952c1d7d73064aabebe0
Ryan Miller <ryan@intranoggin.com>
2025-01-17 13:31:09 -0500

    Updated for blink

--- pi-ConsoleApp2/Program.cs
+++ pi-ConsoleApp2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Device.Gpio;
+using System.Threading;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("VS - Hello, World!");
+
+GpioController controller = new GpioController(PinNumberingScheme.Board);
+
+var pin = 5;
+var lightTime = 300;
+
+controller.OpenPin(pin, PinMode.Output);
+
+try
+{
+    while (true)
+    {
+        controller.Write(pin, PinValue.High);
+        Thread.Sleep(lightTime);
+        controller.Write(pin, PinValue.Low);
+        Thread.Sleep(lightTime);
+    }
+}
+finally
+{
+    controller.ClosePin(pin);
+}
